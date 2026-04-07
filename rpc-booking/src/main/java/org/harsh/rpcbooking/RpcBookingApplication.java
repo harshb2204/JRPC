4ffb7eac873365fb96a61a941ace4150cb0354aa
Harsh <harshb2204@gmail.com>
2026-04-07 22:12:15 +0530

    feat: initialize rpc-booking, rpc-user, and rpc-common microservices with Maven wrapper support

---
+++ rpc-booking/src/main/java/org/harsh/rpcbooking/RpcBookingApplication.java
@@ -0,0 +1,13 @@
+package org.harsh.rpcbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpcBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RpcBookingApplication.class, args);
+    }
+
+}
