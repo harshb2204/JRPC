136ac95b1631e84198910aa380cd4f960d373560
Harsh <harshb2204@gmail.com>
2026-04-09 12:37:46 +0530

    feat: enable RPC client configuration in RpcBookingApplication

--- rpc-booking/src/main/java/org/harsh/rpcbooking/RpcBookingApplication.java
+++ rpc-booking/src/main/java/org/harsh/rpcbooking/RpcBookingApplication.java
@@ -1,9 +1,13 @@
 package org.harsh.rpcbooking;
 
+import com.harsh.rpc.config.EnableRpcClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableRpcClient
 @SpringBootApplication
+// When spring application starts it scans your annotation and it can scan the import
+// RpcClient is instantiated
 public class RpcBookingApplication {
 
     public static void main(String[] args) {
@@ -11,3 +15,5 @@ public class RpcBookingApplication {
     }
 
 }
+// WHen this spring application starts, he wants this application to be a RPC client
+// Need to import rpc client in this module
