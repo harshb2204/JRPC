3d9a4699f86f94c442dd100adfe54d8c458f9ebf
Harsh <harshb2204@gmail.com>
2026-04-14 19:03:56 +0530

    base marker interface

--- rpc/src/main/java/com/harsh/rpc/config/RemoteService.java
+++ rpc/src/main/java/com/harsh/rpc/config/RemoteService.java
@@ -1,4 +1,8 @@
 package com.harsh.rpc.config;
 
-public class RemoteService {
+// Base Marker interface for all Rpc services
+public interface RemoteService {
 }
+// When the framework scans packages looking for RPC services, this is the
+// filter. If a class implements (or extends) RemoteService,
+// it gets picked up. If not, it's ignored.
