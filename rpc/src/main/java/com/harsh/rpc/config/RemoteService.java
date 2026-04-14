1f65d6976fadb3f1c69e2f7202a0d0a4bf6d51c8
Harsh <harshb2204@gmail.com>
2026-04-14 19:05:06 +0530

    base marker interface

--- rpc/src/main/java/com/harsh/rpc/config/RemoteService.java
+++ rpc/src/main/java/com/harsh/rpc/config/RemoteService.java
@@ -6,3 +6,13 @@ public interface RemoteService {
 // When the framework scans packages looking for RPC services, this is the
 // filter. If a class implements (or extends) RemoteService,
 // it gets picked up. If not, it's ignored.
+
+/*
+ * A.isAssignableFrom(B)
+ * // means: "can B be assigned to a variable of type A?"
+ * // equivalently: "is B a subtype of A?"
+ * 
+ * 
+ * RemoteService.class.isAssignableFrom(BookingDetailService.class) // true
+ * RemoteService.class.isAssignableFrom(String.class) // false
+ */
