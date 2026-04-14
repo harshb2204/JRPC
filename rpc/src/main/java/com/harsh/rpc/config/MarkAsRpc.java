4070db09c90a975d847d86dfb5a79f79254428ef
Harsh <harshb2204@gmail.com>
2026-04-14 19:11:12 +0530

    annotation for marking a method as rpc

--- rpc/src/main/java/com/harsh/rpc/config/MarkAsRpc.java
+++ rpc/src/main/java/com/harsh/rpc/config/MarkAsRpc.java
@@ -1,10 +1,11 @@
 package com.harsh.rpc.config;
 
-
 import java.lang.annotation.*;
 
-@Documented
-@Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.METHOD})
+@Documented // show up in javadoc when generating docs
+@Retention(RetentionPolicy.RUNTIME) // controls how long the annotation survives
+@Target(ElementType.METHOD) // restricts where this annotation can be placed
 public @interface MarkAsRpc {
 }
+
+// readable via reflection at runtime
