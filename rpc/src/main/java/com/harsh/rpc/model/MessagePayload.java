d4b7c38df1cd36ca9a4a4e34013add8a5a6336c9
Harsh <harshb2204@gmail.com>
2026-04-16 11:06:28 +0530

    message payload(rpc req and rpc res)

--- rpc/src/main/java/com/harsh/rpc/model/MessagePayload.java
+++ rpc/src/main/java/com/harsh/rpc/model/MessagePayload.java
@@ -1,45 +1,43 @@
 package com.harsh.rpc.model;
 
-import com.alibaba.fastjson.annotation.JSONField;
-import org.apache.coyote.Request;
-import org.apache.coyote.RequestInfo;
-import org.apache.logging.log4j.message.Message;
+import com.alibaba.fastjson2.annotation.JSONField;
 
 import java.io.Serializable;
 
+import javax.swing.Spring;
+
 public class MessagePayload implements Serializable {
-
-    private String clientID;
-
+    // consumer id
+    private String clientId;
 
     @JSONField(serializeUsing = MessageTypeSerializer.class, deserializeUsing = MessageTypeDeserializer.class)
     private MessageType messageType;
+    // Without this, fastjson would serialize the enum as its ordinal number (0, 1,
+    // 2...). With it, it serializes
+    // as the name ("REGISTER", "CALL"...) — which is more readable and safer
 
-    private Object payload; // rpc req or res
+    private Object payload; // RpcRequest or RpcResponse
 
     public MessagePayload() {
-
     }
 
-    public MessagePayload(RequestMessageBuilder builder){
-         this.clientID = builder.clientID;
-         this.messageType = builder.messageType;
-         if(messageType.equals(MessageType.CALL)){
+    public MessagePayload(RequestMessageBuilder builder) {
+        this.clientId = builder.clientId;
+        this.messageType = builder.messageType;
+
+        if (messageType.equals(MessageType.CALL)) {
             this.payload = new RpcRequest(builder);
-         }
-         else if(messageType.equals(MessageType.RESPONSE)){
+        } else if (messageType.equals(MessageType.RESPONSE)) {
             this.payload = new RpcResponse(builder);
-         }
-
-
+        }
     }
 
-    public String getClientID() {
-        return clientID;
+    public String getClientId() {
+        return clientId;
     }
 
-    public void setClientID(String clientID) {
-        this.clientID = clientID;
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
     }
 
     public MessageType getMessageType() {
@@ -58,42 +56,36 @@ public class MessagePayload implements Serializable {
         this.payload = payload;
     }
 
-    // builder pattern
-
+    // Builder Pattern
     public static class RequestMessageBuilder {
-        private String clientID;
-
+        private String clientId;
         private MessageType messageType;
-
         private String requestClientId;
-
-        //uuid
         private String requestId;
-
-        private String requestMethodName;
-
+        private String requestMethodSimpleName;
         private String requestClassName;
-
-        private String returnValueType;
-
-        private String[] paramTypes;
-
+        private String[] paramsTypes;
         private Object[] params;
-
+        private String returnValueType;
         private Object returnValue;
 
-        public RequestMessageBuilder setClientId(String clientID) {
-            this.clientID = clientID;
-            return this;
-        }
-
-        public RequestMessageBuilder setMessageType(MessageType messageType) {
-            this.messageType = messageType;
+        public RequestMessageBuilder clientId(String clientId) {
+            this.clientId = clientId;
             return this;
         }
 
         public RequestMessageBuilder setRequestClientId(String requestClientId) {
             this.requestClientId = requestClientId;
+            return this;
+        }
+
+        public RequestMessageBuilder setReturnValueType(String returnValueType) {
+            this.returnValueType = returnValueType;
+            return this;
+        }
+
+        public RequestMessageBuilder setMessageType(MessageType messageType) {
+            this.messageType = messageType;
             return this;
         }
 
@@ -103,22 +95,17 @@ public class MessagePayload implements Serializable {
         }
 
         public RequestMessageBuilder setRequestMethodName(String requestMethodName) {
-            this.requestMethodName = requestMethodName;
+            this.requestMethodSimpleName = requestMethodName;
             return this;
         }
 
-        public RequestMessageBuilder setRequestClassName(String requestClassName) {
-            this.requestClassName = requestClassName;
-            return this;
-        }
-
-        public RequestMessageBuilder setReturnValueType(String returnValueType) {
-            this.returnValueType = returnValueType;
+        public RequestMessageBuilder setRequestedClassName(String requestedClassName) {
+            this.requestClassName = requestedClassName;
             return this;
         }
 
         public RequestMessageBuilder setParamTypes(String[] paramTypes) {
-            this.paramTypes = paramTypes;
+            this.paramsTypes = paramTypes;
             return this;
         }
 
@@ -133,34 +120,38 @@ public class MessagePayload implements Serializable {
         }
 
         public MessagePayload build() {
-            return new MessagePayload();
+            return new MessagePayload(this);
         }
     }
 
-
-    public static class RpcRequest implements Serializable{
-        // producer id
+    public static class RpcRequest implements Serializable {
+        // Producer id
         private String requestClientId;
-
-        //uuid
+        // unique UUID
         private String requestId;
+        private String requestMethodSimpleName; // method name
 
-        private String requestMethodName;
-
+        // interface class name
+        // The consumer knows the interface (BookingDetailService). The producer uses
+        // this to look up its
+        // Spring bean (BookingDetailServiceImpl) via
+        // applicationContext.getBeansOfType(Class).
         private String requestClassName;
-
         private String returnValueType;
-
-        private String[] paramTypes;
-
+        private String[] paramsTypes;
         private Object[] params;
 
         public RpcRequest() {
-
         }
 
-        public RpcRequest(RequestMessageBuilder builder){
+        public RpcRequest(RequestMessageBuilder builder) {
             this.requestClientId = builder.requestClientId;
+            this.requestId = builder.requestId;
+            this.requestMethodSimpleName = builder.requestMethodSimpleName;
+            this.requestClassName = builder.requestClassName;
+            this.paramsTypes = builder.paramsTypes;
+            this.params = builder.params;
+            this.returnValueType = builder.returnValueType;
         }
 
         public String getRequestClientId() {
@@ -179,12 +170,12 @@ public class MessagePayload implements Serializable {
             this.requestId = requestId;
         }
 
-        public String getRequestMethodName() {
-            return requestMethodName;
+        public String getRequestMethodSimpleName() {
+            return requestMethodSimpleName;
         }
 
-        public void setRequestMethodName(String requestMethodName) {
-            this.requestMethodName = requestMethodName;
+        public void setRequestMethodSimpleName(String requestMethodSimpleName) {
+            this.requestMethodSimpleName = requestMethodSimpleName;
         }
 
         public String getRequestClassName() {
@@ -203,12 +194,12 @@ public class MessagePayload implements Serializable {
             this.returnValueType = returnValueType;
         }
 
-        public String[] getParamTypes() {
-            return paramTypes;
+        public String[] getParamsTypes() {
+            return paramsTypes;
         }
 
-        public void setParamTypes(String[] paramTypes) {
-            this.paramTypes = paramTypes;
+        public void setParamsTypes(String[] paramsTypes) {
+            this.paramsTypes = paramsTypes;
         }
 
         public Object[] getParams() {
@@ -220,15 +211,14 @@ public class MessagePayload implements Serializable {
         }
     }
 
-    public static class RpcResponse implements Serializable{
+    public static class RpcResponse implements Serializable {
         private String requestId;
         private Object result;
 
         public RpcResponse() {
-
         }
 
-        public RpcResponse(RequestMessageBuilder builder){
+        public RpcResponse(RequestMessageBuilder builder) {
             this.requestId = builder.requestId;
             this.result = builder.returnValue;
         }
@@ -249,6 +239,4 @@ public class MessagePayload implements Serializable {
             this.result = result;
         }
     }
-
-
 }
